bfa30e72daa85f2cbdfd3672efe1a2ca1fd8cfd2
piotto <viniciuspiotto.dev@gmail.com>
2024-12-16 09:47:12 -0300

    feat(list-item): increment list items and adjust expense object
    - Incremented the number of items in the list.
    - Made adjustments to the properties of the expense object.

--- scripts.js
+++ scripts.js
@@ -27,6 +27,7 @@ form.addEventListener('submit', (event) => {
 
     const newExpense = {
         id: new Date().getTime(),
+        amount: amount.value,
         expense: expense.value,
         category_id: category.value,
         category_name: category.options[category.selectedIndex].text, // capture a name of selected option
@@ -36,6 +37,9 @@ form.addEventListener('submit', (event) => {
     expenseAdd(newExpense);
 });
 
+/*
+<img src="./img/remove.svg" alt="remover" class="remove-icon" />
+*/
 function expenseAdd(newExpense) {
     try {
         const expenseItem = document.createElement('li');
@@ -44,7 +48,6 @@ function expenseAdd(newExpense) {
         const expenseIcon = document.createElement('img');
         expenseIcon.setAttribute('src', `img/${newExpense.category_id}.svg`);
         expenseIcon.setAttribute('alt', newExpense.category_name);
-        expenseItem.appendChild(expenseIcon);
 
         const expenseInfo = document.createElement('div');
         expenseInfo.classList.add('expense-info');
@@ -54,9 +57,20 @@ function expenseAdd(newExpense) {
         const categoryName = document.createElement('strong');
         categoryName.textContent = newExpense.category_name;
         expenseInfo.appendChild(categoryName);
-        expenseItem.appendChild(expenseInfo);
-        expenseItem.appendChild(expenseInfo);
 
+        const expenseAmount = document.createElement('span');
+        expenseAmount.classList.add('expense-amount');
+        expenseAmount.innerHTML = 
+                                `
+                                    <small>R$</small>${newExpense.amount.toUpperCase().replace("R$", "")}
+                                `;
+
+        const removeIcon = document.createElement('img');
+        removeIcon.classList.add('remove-icon');
+        removeIcon.setAttribute('src', './img/remove.svg');
+        removeIcon.setAttribute('alt', 'remover');
+
+        expenseItem.append(expenseIcon, expenseInfo, expenseAmount, removeIcon);
         expenseList.appendChild(expenseItem);
     } catch (error) {
         alert('Unable to update expense list!');
