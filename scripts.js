135f34978c645ff3bb50f372b11b1cd0dc1cebb6
piotto <viniciuspiotto.dev@gmail.com>
2024-12-15 21:02:55 -0300

    feat(input-handling): capture remaining input values and convert to object
    - Added logic to capture additional input values.
    - Converted captured values into an object structure for further processing.

--- scripts.js
+++ scripts.js
@@ -1,4 +1,7 @@
+const form = document.querySelector('form');
 const amount = document.querySelector('#amount');
+const expense = document.querySelector('#expense');
+const category = document.querySelector('#category');
 
 amount.addEventListener('input', () => {
     let value = amount.value.replace(/\D+/g, '');
@@ -15,4 +18,27 @@ function formatCurrencyBRL(value) {
     });
 
     return value;
+}
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const newExpense = {
+        id: new Date().getTime(),
+        expense: expense.value,
+        category_id: category.value,
+        category_name: category.options[category.selectedIndex].text, // capture a name of selected option
+        created_at: new Date(),
+    };
+
+    expenseAdd(newExpense);
+});
+
+function expenseAdd(newExpense) {
+    try {
+        
+    } catch (error) {
+        alert('Unable to update expense list!');
+        console.log(error);
+    }
 }
